66f33f4bd6ed9de72173aaa7e9a00f3d7ef0b227
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-08-08 11:43:28 +0530

    dummy form created

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -4,7 +4,16 @@ import { getDatabase,push,set,ref } from 'firebase/database'
 export default function Form() {
   return (
     <div>
-      
+      <form action="">
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+      </form>
     </div>
   )
 }
